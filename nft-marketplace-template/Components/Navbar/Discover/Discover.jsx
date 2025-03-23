656ffa57fa44bda8a3f3c99412c28bb4b9ac7b07
Sampreeth <sampreeth.personal@gmail.com>
2025-03-23 15:44:47 +0530

    Worked on backend of individual NavBar components.

--- nft-marketplace-template/Components/Navbar/Discover/Discover.jsx
+++ nft-marketplace-template/Components/Navbar/Discover/Discover.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import Link from 'next/link'
+
+// Internal Imports
+import Styles from './Discover.module.css'
 
 const Discover = () => {
+  // Discover Navigation Menu
+  const discover = [
+    {
+      name: "Collection",
+      link: "collection"
+    },
+    {
+      name: "Search",
+      link: "search"
+    },
+    {
+      name: "Author Profile",
+      link: "author-profile"
+    },
+    {
+      name: "NFT Details",
+      link: "NFT-details"
+    },
+    {
+      name: "Account Settings",
+      link: "account-settings"
+    },
+    {
+      name: "Connect Wallet",
+      link: "connect-wallet"
+    },
+    {
+      name: "Blog",
+      link: "blog"
+    }
+  ];
+
   return (
-    <div>Discover</div>
-  )
-}
+    <div>
+      {discover.map((el, i)=> (
+        <div key={i+1} className={Styles.discover}>
+          <Link href={{pathname: `${el.link}`}}>
+            {el.name}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Discover
--- nft-marketplace-template/Components/Navbar/Notifications/Notifications.jsx
+++ nft-marketplace-template/Components/Navbar/Notifications/Notifications.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+
+//Internal Imports
+import Styles from './Notifications.module.css';
+import images from '../../../img';
 
 const Notifications = () => {
-  return (
-    <div>Notifications</div>
-  )
-}
+  return(
+    <div className={Styles.notification}>
+      <p>Notifications</p>
+      <div className={Styles.notification_box}>
+        <div className={Styles.notification_box_img}>
+          <Image src={images.user1} alt='Profile Image' width={50} height={50} />
+        </div>
+        <div className={Styles.notification_box_info}>
+          <h4>Sampreeth</h4>
+          <p>Message goes in here..</p>
+          <small>30 seconds ago</small>
+        </div>
+        <span className={Styles.notification_box_new}></span>
+      </div>
+    </div>
+  );
+};
 
 export default Notifications
