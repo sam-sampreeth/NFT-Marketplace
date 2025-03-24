3f59677e9802068e8d04111290344b3c5cb429a1
Satvik Joshi <satvikjd@gmail.com>
2025-03-24 21:57:15 +0530

    Completed Hero Section

---
+++ nft-marketplace-template/Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Image from 'next/image';
+
+//INTERNAL IMPORT
+import Style from "./HeroSection.module.css";
+import { Button } from '../componentindex';
+import images from "../../img";
+
+const HeroSection = () => {
+  return (
+    <div className={Style.heroSection}> 
+      <div className={Style.heroSection_box}>
+        <div className={Style.heroSection_box_left}>
+          <h1> Discover, collect and sell NFTs 🖼️ </h1>
+          <p>
+            Discover the most outstanding NFTs in all topics of life .Create your NFTs and sell them
+          </p>
+          <Button btnName ="Start your search "/>
+        </div>
+        <div className={Style.heroSection_box_right}>
+          <Image src ={images.hero} 
+          alt ="hero section "
+           width={600}
+           height ={600} />
+
+        </div>
+      </div>
+     </div>
+  )
+}
+
+export default HeroSection
