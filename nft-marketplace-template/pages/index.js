5e5bcf0de557bc3fabcb6f382a9f85aecc98b743
Sampreeth <sampreeth.personal@gmail.com>
2025-03-25 14:19:05 +0530

    Completed Category component

--- nft-marketplace-template/pages/index.js
+++ nft-marketplace-template/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 //INTERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection, Service, BigNFTSlider, Subscribe, Title } from "../Components/componentindex";
+import { HeroSection, Service, BigNFTSlider, Subscribe, Title, Category } from "../Components/componentindex";
 
 const Home = () => {
   return (
@@ -11,6 +11,7 @@ const Home = () => {
       <Service />
       <BigNFTSlider />
       <Title heading="Browse by Category" paragraph="Explore the best NFTs in our featured categories"/>
+      <Category />
       <Subscribe />
     </div>
   );
